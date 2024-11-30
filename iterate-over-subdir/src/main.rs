063e78daf9b97ffea0a1eec02da348a1454b864a
Wink Saville <wink@saville.com>
2024-11-29 16:56:56 -0800

    feat: Add logging using my custom_logger
    So it is running and calling the commits_for_subdir, but still isn't printing any real results
    so need to add more logging, but we've got visibility now:
    
      $ cargo run -- ~/prgs/rust/myrepos/expr-rust-git2 ~/prgs/rust/myrepos/expr-rust-git2
         Compiling iterate-over-subdir v0.1.0 (/home/wink/prgs/rust/myrepos/expr-rust-git2/iterate-over-subdir)
          Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.89s
           Running `/home/wink/prgs/rust/myrepos/expr-rust-git2/target/debug/iterate-over-subdir /home/wink/prgs/rust/myrepos/expr-rust-git2 /home/wink/prgs/rust/myrepos/expr-rust-git2`
      [2024-11-30T00:53:52.155215451Z INFO  iterate_over_subdir   75  1] main:+
      [2024-11-30T00:53:52.155255205Z INFO  iterate_over_subdir    6  1] commits_for_subdir:+ repo_path: /home/wink/prgs/rust/myrepos/expr-rust-git2, subdir: /home/wink/prgs/rust/myrepos/expr-rust-git2
      [2024-11-30T00:53:52.163311981Z INFO  iterate_over_subdir   96  1] main:-

--- iterate-over-subdir/src/main.rs
+++ iterate-over-subdir/src/main.rs
@@ -3,6 +3,7 @@ use std::env;
 use git2::{Repository, Tree, Error};
 
 fn commits_for_subdir(repo_path: &str, subdir: &str) -> Result<(), Error> {
+    log::info!("commits_for_subdir:+ repo_path: {repo_path}, subdir: {subdir}");
     let repo = Repository::open(repo_path)?;
     let mut revwalk = repo.revwalk()?;
 
@@ -69,6 +70,9 @@ fn usage() {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    custom_logger::env_logger_init("info");
+
+    log::info!("main:+");
     let repo_path = match env::args().nth(1) {
         Some(path) => path,
         None => {
@@ -84,8 +88,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     };
     if let Err(e) = commits_for_subdir(&repo_path, &subdir_path) {
+        //eprintln!("Error: {}", e);
+        log::info!("main: Error: {e}");
         return Err(e.into());
     }
 
+    log::info!("main:-");
     Ok(())
 }
