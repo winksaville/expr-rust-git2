ab7a730d6657def3b3be87a97988e6993a9ab0bf
Wink Saville <wink@saville.com>
2024-12-01 10:38:21 -0800

    feat: Use diff_tree_to_tree to determine if a change was in a subdir
    This is my unshared private link to the GPT4o:
      https://chatgpt.com/c/674a9111-d748-800c-8cbc-4f230eea64e6

--- iterate-over-subdir/src/main.rs
+++ iterate-over-subdir/src/main.rs
@@ -1,37 +1,72 @@
 use std::env;
 
-use git2::{Repository, Error};
+use git2::{Repository, Error, Tree, DiffOptions, Oid, Commit};
+
+//fn commits_for_subdir(repo_path: &str, subdir: &str) -> Result<(), Error> {
+//    log::info!("commits_for_subdir:+ repo_path: {repo_path}, subdir: {subdir}");
+//    let repo = Repository::open(repo_path)?;
+//    let mut revwalk = repo.revwalk()?;
+//
+//    // Push HEAD to start traversal
+//    revwalk.push_head()?;
+//
+//    // Sort commits by in topological order. This is different from the default,
+//    // which is git2::Sort::NONE. With NONE the order is unspecified and may change
+//    // according the comment for NONE in git2::Sort.
+//    revwalk.set_sorting(git2::Sort::TOPOLOGICAL).unwrap();
+//
+//    let subdir_is_root = subdir == "/";
+//    log::info!("subdir_is_root: {subdir_is_root}");
+//
+//    for oid_result in revwalk {
+//        let oid = oid_result?;
+//        let commit = repo.find_commit(oid)?;
+//        log::info!("Processing commit: {}", commit.id());
+//
+//        // Get the tree of the current commit
+//        let tree = commit.tree()?;
+//        log::info!("Tree: {}", tree.id());
+//
+//        if subdir_is_root {
+//            // Always print for root
+//            println!("{}: {}", commit.id(), commit.summary().unwrap_or("No summary"));
+//        } else if tree.get_path(std::path::Path::new(subdir)).is_ok() {
+//            // Print only if the subdirectory exists
+//            println!("{}: {}", commit.id(), commit.summary().unwrap_or("No summary"));
+//        }
+//    }
+//
+//    log::info!("commits_for_subdir:- repo_path: {repo_path}, subdir: {subdir}");
+//    Ok(())
+//}
 
 fn commits_for_subdir(repo_path: &str, subdir: &str) -> Result<(), Error> {
     log::info!("commits_for_subdir:+ repo_path: {repo_path}, subdir: {subdir}");
     let repo = Repository::open(repo_path)?;
     let mut revwalk = repo.revwalk()?;
-
-    // Push HEAD to start traversal
     revwalk.push_head()?;
-
-    // Sort commits by in topological order. This is different from the default,
-    // which is git2::Sort::NONE. With NONE the order is unspecified and may change
-    // according the comment for NONE in git2::Sort.
-    revwalk.set_sorting(git2::Sort::TOPOLOGICAL).unwrap();
-
-    let subdir_is_root = subdir == "/";
-    log::info!("subdir_is_root: {subdir_is_root}");
+    revwalk.set_sorting(git2::Sort::TIME)?;
 
     for oid_result in revwalk {
-        let oid = oid_result?;
-        let commit = repo.find_commit(oid)?;
-        log::info!("Processing commit: {}", commit.id());
+        let oid: Oid = oid_result?;
+        let commit: Commit = repo.find_commit(oid)?;
+        let tree: Tree = commit.tree()?;
+        log::info!("Processing commit: id: {} tree: {tree:?}", commit.id());
 
-        // Get the tree of the current commit
-        let tree = commit.tree()?;
-        log::info!("Tree: {}", tree.id());
+        let parent_tree: Option<Tree> = if let Some(parent) = commit.parents().next() {
+            Some(parent.tree()?)
+        } else {
+            None
+        };
+        log::info!("Processing commit: parent_tree: {parent_tree:?}");
 
-        if subdir_is_root {
-            // Always print for root
-            println!("{}: {}", commit.id(), commit.summary().unwrap_or("No summary"));
-        } else if tree.get_path(std::path::Path::new(subdir)).is_ok() {
-            // Print only if the subdirectory exists
+        // Move outside of loop?
+        let mut diff_opts = DiffOptions::new();
+        diff_opts.pathspec(subdir);
+
+        let diff = repo.diff_tree_to_tree(parent_tree.as_ref(), Some(&tree), Some(&mut diff_opts))?;
+        log::info!("Processing commit: diff.deltas().len(): {}", diff.deltas().len());
+        if diff.deltas().len() > 0 {
             println!("{}: {}", commit.id(), commit.summary().unwrap_or("No summary"));
         }
     }
