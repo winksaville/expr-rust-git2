002d753dfc23c31b897b1ba960c763bd31d89b9b
Wink Saville <wink@saville.com>
2024-12-12 09:54:53 -0800

    feat: Handle merge commits
    This is the initial suggestion from
    [ChatGPT4o](https://chatgpt.com/share/675c98e7-4d38-800c-a0a8-89c7f0d1e334)
    
    Does not compile

--- get-commits/src/main.rs
+++ get-commits/src/main.rs
@@ -1,79 +1,98 @@
 use std::{env, error::Error};
+use git2::{Commit, Diff, Oid, Repository, Tree};
 
-use git2::Repository;
+fn collect_diff_files(
+    repo: &Repository,
+    from_tree: Option<&Tree>,
+    to_tree: Option<&Tree>,
+    modified_files: &mut Vec<String>,
+    non_matching_count: &mut usize,
+) -> Result<(), Box<dyn Error>> {
+    let diff = repo.diff_tree_to_tree(from_tree, to_tree, None)?;
+    for delta in diff.deltas() {
+        if let Some(path) = delta.new_file().path() {
+            let path_str = path.to_string_lossy();
+            if path_str.starts_with("") { // Adjust for subdir filtering
+                modified_files.push(path_str.to_string());
+            } else {
+                *non_matching_count += 1;
+            }
+        }
+    }
+    Ok(())
+}
 
-fn get_commits_between(repo_path: &str, oid_strings: &Vec<String>) -> Result<(), Box<dyn Error>> {
-    log::info!("get_commits_between:+ repo_path: {repo_path}, oid_strings: {oid_strings:?}");
-    let result = Repository::open(repo_path);
-    log::info!(
-        "get_commits_between: Repository::open result: is_ok={}",
-        result.is_ok()
-    );
-    let repo = result?;
+fn create_merged_baseline_tree(repo: &Repository, parents: &[Commit]) -> Result<Tree, Box<dyn Error>> {
+    let mut index = repo.index()?;
+    index.read_tree(&parents[0].tree()?)?;
+    for parent in &parents[1..] {
+        let parent_tree = parent.tree()?;
+        repo.merge_trees(index.write_tree_to(repo)?.as_object(), parent_tree.as_object(), None)?;
+        index.read_tree(&parent_tree)?;
+    }
+    Ok(repo.find_tree(index.write_tree_to(repo)?)?)
+}
 
-    let mut oids = Vec::new();
-    for oid_str in oid_strings {
-        let oid = git2::Oid::from_str(oid_str)?;
-        oids.push(oid);
+fn process_commit(repo: &Repository, commit: &Commit) -> Result<(), Box<dyn Error>> {
+    let commit_tree = commit.tree()?;
+    let parents = commit.parents().collect::<Vec<_>>();
+
+    let mut modified_files = Vec::new();
+    let mut non_matching_count = 0;
+
+    if parents.len() > 1 {
+        // Handle merge commits
+        let merged_baseline_tree = create_merged_baseline_tree(repo, &parents)?;
+        collect_diff_files(repo, Some(&merged_baseline_tree), Some(&commit_tree), &mut modified_files, &mut non_matching_count)?;
+    } else if let Some(parent) = parents.first() {
+        // Handle non-merge commits
+        let parent_tree = parent.tree()?;
+        collect_diff_files(repo, Some(&parent_tree), Some(&commit_tree), &mut modified_files, &mut non_matching_count)?;
     }
 
-    if oids.is_empty() || oids.len() > 2 {
-        return Err(format!("We need one or two oids, there were {}", oids.len()).into());
+    println!(
+        "commit id: {}, summary: '{}', parent.len: {}, modified_count: {}, non_matching_count: {}, parents: {:?}",
+        commit.id(),
+        commit.summary().unwrap_or(""),
+        parents.len(),
+        modified_files.len(),
+        non_matching_count,
+        parents.iter().map(|p| p.id()).collect::<Vec<_>>(),
+    );
+
+    for file in &modified_files {
+        println!("  file: {}", file);
+    }
+
+    Ok(())
+}
+
+fn get_commits(repo_path: &str, oid_strings: &[String]) -> Result<(), Box<dyn Error>> {
+    log::info!("get_commits:+ repo_path: {repo_path}, oid_strings: {oid_strings:?}");
+    let repo = Repository::open(repo_path)?;
+
+    let oids = oid_strings
+        .iter()
+        .map(|oid_str| Oid::from_str(oid_str))
+        .collect::<Result<Vec<_>, _>>()?;
+
+    if oids.len() < 1 || oids.len() > 2 {
+        return Err(format!("We need one or two oids, but got {}", oids.len()).into());
     }
 
     let mut revwalk = repo.revwalk()?;
     revwalk.set_sorting(git2::Sort::TOPOLOGICAL)?;
-
-    // Push the starting commit and hide the ending commit if present
     revwalk.push(oids[0])?;
     if oids.len() == 2 {
         revwalk.hide(oids[1])?;
     }
 
-    //let commits = revwalk.filter_map(Result::ok).collect::<Vec<_>>();
-    for commit in revwalk.filter_map(Result::ok) {
-        let commit = repo.find_commit(commit)?;
-        let commit_tree = commit.tree()?;
-        let parents = commit.parents().collect::<Vec<_>>();
-
-        let mut has_patch = false;
-        let mut modified_files = Vec::new();
-        let mut non_matching_count = 0;
-
-        for parent in &parents {
-            let parent_tree = parent.tree()?;
-            let diff = repo.diff_tree_to_tree(Some(&parent_tree), Some(&commit_tree), None)?;
-
-            for delta in diff.deltas() {
-                has_patch = true;
-                if let Some(path) = delta.new_file().path() {
-                    let path_str = path.to_string_lossy();
-                    if path_str.starts_with("") { // ATM match all files in the future take a subdir parameter
-                        modified_files.push(path_str.to_string());
-                    } else {
-                        non_matching_count += 1;
-                    }
-                }
-            }
-        }
-
-        println!(
-            "commit id: {}, summary: '{}', parent.len: {}, has_patch: {}, modified_count {:?}, non_matching_count: {}, parents: {:?}",
-            commit.id(),
-            commit.summary().unwrap_or(""),
-            parents.len(),
-            has_patch,
-            modified_files.len(),
-            non_matching_count,
-            parents.iter().map(|p| p.id()).collect::<Vec<_>>(),
-        );
-
-        for file in modified_files {
-            println!("  file: {}", file);
-        }
+    for oid in revwalk.filter_map(Result::ok) {
+        let commit = repo.find_commit(oid)?;
+        process_commit(&repo, &commit)?;
     }
 
-    log::info!("get_commits_between:- repo_path: {repo_path}, oid_strings: {oid_strings:?}");
+    log::info!("get_commits:- repo_path: {repo_path}, oid_strings: {oid_strings:?}");
     Ok(())
 }
 
@@ -90,27 +109,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     log::info!("main:+");
 
     let mut args_iter = env::args();
+    args_iter.next(); // Skip executable name
 
-    _ = args_iter.next(); // Skip executable name the first parameter
+    let repo_path = args_iter.next().ok_or_else(|| "Repository path not provided")?;
+    let oid_strings: Vec<_> = args_iter.collect();
 
-    // Get the repository path which is the first argument
-    let repo_path = match args_iter.next() {
-        Some(path) => path,
-        None => {
-            usage();
-            return Err("Repository path not provided".into());
-        }
-    };
-
-    // Get the OIDs
-    let mut oid_strings = Vec::new();
-    for arg in args_iter {
-        log::info!("arg: {arg}");
-        oid_strings.push(arg);
+    if oid_strings.is_empty() {
+        usage();
+        return Err("OIDs not provided".into());
     }
 
-    // Get the commits between the OIDs
-    let result = get_commits_between(&repo_path, &oid_strings);
+    let result = get_commits(&repo_path, &oid_strings);
 
     log::info!("main:-");
     result
